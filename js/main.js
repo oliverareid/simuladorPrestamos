530c4fdcc1b90fcfeef172e073f1b5b6355240dd
Franco Olivera Reid <foliverareid@gmail.com>
2022-07-28 19:12:16 -0300

    simulador de préstamos

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let boton = document.getElementById("btnCalcular");
+boton.addEventListener("click", generarTabla);
+
+function generarTabla(){
+    document.getElementById("tab").innerHTML="";
+    let capital = Number(document.getElementById("capital").value);
+    let cuota = Number(document.getElementById("cuota").value);
+    let interes = Number(document.getElementById("interes").value);
+    if(capital>0){   
+        for(i=1;i<=cuota;i++){
+            let montoPorMes = capital/cuota;
+            let montoPorMesStr = montoPorMes.toFixed(2);
+            let interesesPorMes = ((capital*interes)/100)/cuota;
+            let interesesPorMesStr = interesesPorMes.toFixed(2);
+            let totalMes = montoPorMes + interesesPorMes;
+            let totalMesStr = totalMes.toFixed(2);
+            document.getElementById("tab").innerHTML = document.getElementById("tab").innerHTML+
+                    `<tr>
+                        <td> ${i}</td>
+                        <td> ${montoPorMesStr}</td>
+                        <td> ${interesesPorMesStr}</td>
+                        <td> ${totalMesStr}</td>
+                    </tr>`;
+        }
+        let capitalStr = capital.toFixed(2);
+        let totalIntereses = interesesPorMes*cuota;
+        let totalInteresesStr = totalIntereses.toFixed(2);
+        let total = totalMes*cuota;
+        let totalStr= total.toFixed(2);
+        document.getElementById("t1").innerHTML = capitalStr;
+        document.getElementById("t2").innerHTML = totalInteresesStr;
+        document.getElementById("t3").innerHTML = totalStr;        
+    }else{
+        alert("Falta ingresar un Número");
+    }
+}
